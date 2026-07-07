2a4e21102b1b2eabcac6c2db298156fb6ef82aaf
League Student <student@jointheleague.org>
2026-07-07 12:57:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
+++ lessons/10_Turtles/90_Graphics_Projects/20_Crazy_Spiral.py
@@ -7,15 +7,15 @@ in 10_Flaming_Ninja_Star.py, but use what you've learned about loops
 uid: zfzMbyH7
 name: Crazy Spiral
 """
-def get_random_color():
-    return "#%06X" % (random.randint(0, 0xFFFFFF))
-
 import turtle
+import random
 t = turtle.Turtle()
 turtle.setup(600, 600, 0, 0)            # Set the size of the window
 window = turtle.Screen()
 left = 90
 forward = 2
+def get_random_color():
+    return "#%06X" % (random.randint(0, 0xFFFFFF))
 while True:
     t.left(left)
     t.forward(forward)
