41ff795adaf6069009a28e32273e9d81af20e297
League Student <student@jointheleague.org>
2026-07-10 12:46:01 -0700

    Auto-commit: workspace changes before push

--- lessons/10_Turtles/20_Introducing_Tina/10_Meet_Tina/Meet_Tina.py
+++ lessons/10_Turtles/20_Introducing_Tina/10_Meet_Tina/Meet_Tina.py
@@ -6,8 +6,8 @@ screen.setup(500, 500)
 cam_colors = ["white", "blue", "red", "black", "green"]
 
 def OpenCam(number):
-    print ("Cam 1")
+    print("Cam 1")
     screen.bgcolor(cam_colors[number - 1])
 
 screen.listen()
-screen.onkey(OpenCam(1), 1)
+screen.onkey(OpenCam(1), "1")
